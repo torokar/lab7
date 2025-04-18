79f63ccc93543dc84eba98dd9cbd8ed52e089266
Andrey <andrey.onkhonov95@bk.ru>
2025-04-19 00:50:12 +0800

    битовая операция

---
+++ bit_oper/bit_oper.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+
+int main() {
+    int n;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+    std::cout << (2 << n) << std::endl;
+
+    std::cout << "Введите m: ";
+    int m;
+    std::cin >> m;
+    std::cout << (2 << n )+( 2 << m);
+
+    return 0;
+}
